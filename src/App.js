9766f4d6e61856b15c3b3f84220e47c01edae04e
Wasek <wesinem@gmail.com>
2020-06-24 23:08:01 -0700

    added directory and shoppage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header/header.component';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import CheckoutPage from './pages/checkoutpage/checkoutpage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
         <Route exact path='/checkout' component={CheckoutPage} />
       </Switch>
     </div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,40 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import ListItem from '../../components/list-item/list-item.component';
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
-class HomePage extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            items: []
-        };
-    }
-
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/photos')
-            .then(response => response.json())
-            .then(photos => {
-                return photos.map((item) => {
-                    const price = Math.floor(Math.random() * 200) + 10; // price ranges from 10 to 200
-                    return Object.assign({}, item, { price });
-                })
-            })
-            .then(newPhotos => { this.setState({ items: newPhotos }) });
-    }
-
-    render() {
-        const { items } = this.state;
-
-        return (
-            <div className='homepage-container'>
-                {items.map(( item ) => (
-                    <ListItem key={item.id} item={item} />
-                ))}
-            </div>
-        );
-    }
-}
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
 
 export default HomePage;
