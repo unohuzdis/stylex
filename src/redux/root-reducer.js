513169c90891f1ce50b8ee88d7516b5bcb874079
Wasek <wesinem@gmail.com>
2020-06-26 00:09:17 -0700

    added directory data into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,7 +4,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-// import directoryReducer from './directory/directory.reducer';
+import directoryReducer from './directory/directory.reducer';
 // import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
@@ -15,8 +15,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
-//   directory: directoryReducer,
+  cart: cartReducer,
+  directory: directoryReducer,
 //   shop: shopReducer
 });
 
